d26ca35f4bfe1a9b8715bafcd08835948553cb6d
MarcusStjerno <90749370+MarcusStjerno@users.noreply.github.com>
2023-10-26 12:40:23 +0200

    Update User + fix error

--- DataLayer/Objects/User.cs
+++ DataLayer/Objects/User.cs
@@ -10,12 +10,13 @@ namespace DataLayer.Objects
     public class User
     {
         [Key]
-        public int Id { get; set; }
+        public string Id { get; set; }
         public string Username { get; set; }
-        public string Password { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Dob { get; set; } //Date of Birth
-        public string Email { get; set; }
+        //    public string Password { get; set; }
+        //    public string FirstName { get; set; }
+        //    public string LastName { get; set; }
+        //    public string Dob { get; set; } //Date of Birth
+        //    public string Email { get; set; }
+        //}
     }
 }
