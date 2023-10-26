ce4ab0c16980c2141662457c75aed8fb0ea3de1e
MarcusStjerno <90749370+MarcusStjerno@users.noreply.github.com>
2023-10-26 11:51:28 +0200

    User fix

--- DataLayer/Objects/User.cs
+++ DataLayer/Objects/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,11 @@ namespace DataLayer.Objects
 {
     public class User
     {
+        [Key]
         public int Id { get; set; }
-        public string? Username { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
 }
